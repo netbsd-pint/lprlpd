05dd6a5eebe151af015e9162f3f627b87bf0a6f1
pierceg2 <pierceg2@students.wwu.edu>
2017-04-20 14:51:18 -0700

    This should have been with my last commit! Goes with renaming and such.

--- src/lpr/main.c
+++ src/lpr/main.c
@@ -8,10 +8,6 @@
 #include "lpr_job.h"
 #include "../common/common.h"
 
-
-
-static int	 userid;	/* user id */
-
 static void
 usage(void)
 {
@@ -23,14 +19,18 @@ usage(void)
   exit(1);
 }
 
-lpr_flags *
+struct lpr_flags *
 parse_commandline (int argc, char **argv)
 {
-  lpr_flags *j = new_lpr_flags ("user1", "localhost");
+  struct lpr_flags *j = new_lpr_flags ("user1", "localhost");
   extern char *optarg;
   extern int optind;
   int bflag, fd;
   char ch[2] = {0};
+
+  if (!j) {
+    exit (1);
+  }
 
   while ((ch[0] = getopt (argc, argv, "#:1:2:3:4:J:T:U:C:i:cdfghlmnopPqrRstv")) != -1) {
     switch (ch[0]) {
@@ -59,58 +59,24 @@ parse_commandline (int argc, char **argv)
     case 'i':
       j->iflag = atoi (optarg);
       break;
-      /* the rest of these flags are booleans */
-    case 'c':
-      j->cflag = true;
-      break;
-    case 'd':
-      j->dflag = true;
-      break;
-    case 'f':
-      j->fflag = true;
-      break;
-    case 'g':
-      j->gflag = true;
-      break;
-    case 'h':
-      j->hflag = true;
-      break;
-    case 'l':
-      j->lflag = true;
-      break;
-    case 'm':
-      j->mflag = true;
-      break;
-    case 'n':
-      j->nflag = true;
-      break;
-    case 'o':
-      j->oflag = true;
-      break;
-    case 'p':
-      j->pflag = true;
-      break;
-    case 'P':
-      j->Pflag = true;
-      break;
-    case 'q':
-      j->qflag = true;
-      break;
-    case 'r':
-      j->rflag = true;
-      break;
-    case 'R':
-      j->Rflag = true;
-      break;
-    case 's':
-      j->sflag = true;
-      break;
-    case 't':
-      j->tflag = true;
-      break;
-    case 'v':
-      j->vflag = true;
-      break;
+    /* the rest of these flags are booleans */
+    case 'c': j->cflag = true; break;
+    case 'd': j->dflag = true; break;
+    case 'f': j->fflag = true; break;
+    case 'g': j->gflag = true; break;
+    case 'h': j->hflag = true; break;
+    case 'l': j->lflag = true; break;
+    case 'm': j->mflag = true; break;
+    case 'n': j->nflag = true; break;
+    case 'o': j->oflag = true; break;
+    case 'p': j->pflag = true; break;
+    case 'P': j->Pflag = true; break;
+    case 'q': j->qflag = true; break;
+    case 'r': j->rflag = true; break;
+    case 'R': j->Rflag = true; break;
+    case 's': j->sflag = true; break;
+    case 't': j->tflag = true; break;
+    case 'v': j->vflag = true; break;
     case '?':
     default:
       usage ();
@@ -121,7 +87,7 @@ parse_commandline (int argc, char **argv)
 
   /* The last argument(s) will be filename(s) */
   // TODO: Make this a while loop that supports multiple files
-  // Also, reminder that the `protocolize` functio will iterate over files, copies
+  // Also, reminder that the `protocolize` function will iterate over files, copies
   // making a separate packet for each copy of each file (copies * files) packets
   if (optind <= argc) {
     j->filename = (char *) malloc (strlen (argv[optind]));
@@ -136,35 +102,41 @@ parse_commandline (int argc, char **argv)
 int
 main (int argc, char **argv)
 {
-  int status = 0;
+  int userid = -1;
+  int printer_status = -1;
+  char hostname[256] = {0};
+  char *printername = NULL;
+  struct lpr_flags *flags = NULL;
+  struct printer *printcap = NULL;
 
-  setprogname(*argv);/*Therefore, in NetBSD, calling setprogname() 				    explicitly has no effect.
-                However, portable programs that wish to use 				    getprogname() should call
-                setprogname() from main().
-            On operating systems where getprogname() and
-                setprogname() are implemented
-            via a portability library, this call is
-                needed to make the name available.*/
+  /* Therefore, in NetBSD, calling setprogname() explicitly has no effect.
+     However, portable programs that wish to use getprogname() should call
+     setprogname() from main(). On operating systems where getprogname() and
+     setprogname() are implemented via a portability library, this call is
+     needed to make the name available.
+  */
+  setprogname(*argv);
+  userid = getuid();
+  gethostname(host, 256);
+  flags = parse_commandline (argc, argv);
 
+  printername = getenv ("PRINTER");
+  if (!printername) {
+    printername = DEFAULT_PRINTER;
+  }
+  printf ("'%s'\n", printername);
+  printcap = new_printer (printername);
 
-  gethostname(host, sizeof (host)); //host machine's name
-  userid = getuid();                //user's name
-  lpr_flags *j = parse_commandline (argc, argv);
+  if (!flags || !printcap) {
+    exit (1);
+  }
 
-  /* Printcap setup */
-  struct printer_st *printer = (struct printer_st*) malloc (sizeof (struct printer_st));
-  printer->name = "lp";
-  getprintcap(printer);
+  /* Attempt to load printer configuration data from printcap */
+  printer_status = getprintcap (printcap);
+  if (printer_status < 0) {
+    printf ("Printer %s does not exists in printcap.\n", printcap->name);
+    exit (1);
+  }
 
-  int rnum = random();
-  rnum = rnum %1000;
-
-
-
-  // so we should be calling this after setting flags...
-  //void getprintcap(struct printer_st *printer)
-
-
-
-  return status;
+  return 0;
 }
