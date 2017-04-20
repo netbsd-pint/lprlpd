dc93e26d028e6edc1f0a553d53fd57406d0bd9cd
mcgrewz <mcgrewz@students.wwu.edu>
2017-04-20 16:54:19 -0700

    print_job.h: Add new header describing the print job structure
    This is an early version, and definitely not complete.
    
    It also defines the print_extra structure, which will probably need to be massively expanded.

---
+++ src/common/print_job.h
@@ -0,0 +1,30 @@
+#ifndef PRINT_JOB_H
+#define PRINT_JOB_H
+
+#include "common.h"
+
+struct print_extra {
+  enum font font_type;
+  bool print_control_chars;
+};
+
+struct job {
+  char **file_names;
+  char **mime_types;
+
+  char *email;
+  char *username;
+  char *hostname;
+  char *job_id;
+  char *job_name;
+
+  struct printer* p;
+
+  void *extra;
+  
+  size_t copies;
+
+  bool burst_page;
+};
+
+#endif
