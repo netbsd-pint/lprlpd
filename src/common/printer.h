0bd910d6e5876cf292fe0c968ddbea17ee9eb3cb
mcgrewz <mcgrewz@students.wwu.edu>
2017-05-01 11:44:39 -0700

    Fix compile after refactor

---
+++ src/common/printer.h
@@ -0,0 +1,21 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+#include <stdbool.h>
+
+struct printer {
+  char* local_printer;
+  char* lock_file;
+  char* log_file;
+  char* name;
+  char* remote_printer;
+  char* spooling_dir;
+  char* status_file;
+  char* restr_group;
+  long max_file_size;
+  int protocol;
+  bool mult_copies;
+  char pad[3]; /* padding to appease the compiler clang! */
+};
+
+#endif
